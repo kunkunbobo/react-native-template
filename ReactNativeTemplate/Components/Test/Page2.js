b42e76f8a2b4bd654a392c207f8edb41ef8dee9c
jean <jean.ma.1986@gmail.com>
2016-08-16 16:53:08 +0800

    test rest client

--- ReactNativeTemplate/Components/Test/Page2.js
+++ ReactNativeTemplate/Components/Test/Page2.js
@@ -5,6 +5,7 @@ import Button from "react-native-button";
 import {Actions} from "react-native-router-flux";
 import {connect} from "react-redux";
 import TestActions from "../../Actions/Test/Test.action.js";
+import restClient from "../../Utility/RestClient.js";
 
 
 class Page2 extends BC{
@@ -19,6 +20,13 @@ class Page2 extends BC{
 				}}>Trigger Hello</Button>
 				<Text style={[TestStyles.Text]}>message from router params : {this.props.Message}</Text>
 				<Text style={[TestStyles.Text]}>message from store : {this.props.message}</Text>
+				<Button style={TestStyles.Button} onPress={()=>{
+					restClient.get("http://lwapp.yzw.cn/home/GetHomeInfo").then((response)=>{
+						console.log(response.body);
+					}).catch((err)=>{
+						console.log(err);
+					});
+				}}>Test Rest Client</Button>
 			</View>
 		);
 	}
