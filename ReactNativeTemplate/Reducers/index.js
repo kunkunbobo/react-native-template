ef8ffb7ac96f38ac9554b2377778fad5239b49da
jean <jean.ma.1986@gmail.com>
2016-08-17 09:28:40 +0800

    fixed

--- ReactNativeTemplate/Reducers/index.js
+++ ReactNativeTemplate/Reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import routerReducer from "./router.reducer.js";
+import routerReducer from "./Router.reducer.js";
 import testReducer from "./Test.reducer.js";
 
 export default combineReducers({
