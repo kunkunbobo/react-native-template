a7d3903fb01e057bfb52073c5d5a13281533bfe6
youkai <youkai@yzw.cn>
2016-08-18 18:27:14 +0800

    fix

--- ReactNativeTemplate/index.js
+++ ReactNativeTemplate/index.js
@@ -17,7 +17,7 @@ const Store=compose(
 
 const Scenes = Actions.create(
 	<Scene key="root" duration={0}>
-		<Scene key="Page1" component={Page1} title="PageOne" initial={true}></Scene>
+		<Scene key="Page1" component={Page1} title="Page1" initial={true}></Scene>
 		<Scene key="Page2" component={Page2} title="Page2"></Scene>
 		<Scene key="Tab" component={Tab} title="Tab Demo"></Scene>
 	</Scene>
